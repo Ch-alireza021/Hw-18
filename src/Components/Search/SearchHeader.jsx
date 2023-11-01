c25b44af1121c53bd8e31eba08d79b5891fd75f9
Alireza Chizari <ch.alireza021@gmail.com>
2023-11-01 21:06:14 +0330

    fix search

--- src/Components/Search/SearchHeader.jsx
+++ src/Components/Search/SearchHeader.jsx
@@ -1,7 +1,7 @@
 const SearchHeader=({searchInput})=>{
 return(
     <div className="w-full h-full pt-20 ">
-        <input onChange={searchInput} className="bg-[#3B3B3B] px-[38px] outline-none w-full rounded-full h-14" type="search" />
+        <input onChange={searchInput} className="bg-[#3B3B3B] px-[38px] text-[20px] text-white outline-none w-full rounded-full h-14" type="search" />
     </div>
 )
 }
